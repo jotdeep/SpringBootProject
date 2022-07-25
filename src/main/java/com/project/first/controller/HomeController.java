611802c1b1b3d380be0129cf411ec682e5d725d9
Jotdeep Singh <jsahluwalia11@gmail.com>
2022-07-25 19:38:07 +0530

    Created most of the frontend

--- src/main/java/com/project/first/controller/HomeController.java
+++ src/main/java/com/project/first/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.project.first.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,9 +14,27 @@ import com.project.first.entities.User;
 @Controller
 public class HomeController 
 {
-	@RequestMapping("/home")
-	public String home()
+	@RequestMapping("/")
+	public String home(Model model)
 	{
+		model.addAttribute("title","Home - Smart Contact Manager");
 		return "home";
 	}
+	@RequestMapping("/base")
+	public String base()
+	{
+		return "base";
+	}
+	@RequestMapping("/about")
+	public String about()
+	{
+		return "about";
+	}
+	@RequestMapping("/signup")
+	public String signup(Model model)
+	{
+		model.addAttribute("title","Signup - Smart Contact Manager");
+		return "signup";
+	}
 }
+ 
